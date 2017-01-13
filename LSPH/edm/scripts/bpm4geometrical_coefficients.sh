80fe38f12a05ab7d8f9a2d71f4f4a38dfde2b5fb
libera <libera@libera-dev.(none)>
2017-01-13 14:43:08 +0100

    Libera Single Pass H updates.

---
+++ LSPH/edm/scripts/bpm4geometrical_coefficients.sh
@@ -0,0 +1,16 @@
+# Script for setting geometrical coefficients. Please note that first field in the array "16"
+# defines number of fields in the array. Command is composed from the following items
+#
+# caput -a IOC_NAME:BPM_NAME:calibration:k:X_Y_PLANE:FREQ_HARMONIC 16 K0 K1 K2 K3 K4 K5 K6 K7 K8 K9 K10 K11 K12 K13 K14 K15 
+
+
+# KX geometrical coefficients for for frequency harmonic 1
+caput -a LSPH2204:bpm4:calibration:k:x:f1 16 0 0 0 0 10000000 0 0 0 0 0 0 0 0 0 0 0
+# KX geometrical coefficients for for frequency harmonic 2
+caput -a LSPH2204:bpm4:calibration:k:x:f2 16 0 0 0 0 10000000 0 0 0 0 0 0 0 0 0 0 0
+
+
+# KY geometrical coefficients for for frequency harmonic 1
+caput -a LSPH2204:bpm4:calibration:k:y:f1 16 0 10000000 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+# KY geometrical coefficients for for frequency harmonic 2
+caput -a LSPH2204:bpm4:calibration:k:y:f2 16 0 10000000 0 0 0 0 0 0 0 0 0 0 0 0 0 0 
