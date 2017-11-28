1a59cf27642907632693824a5bac17b2213ba110
Peter Leban <peter.leban@i-tech.si>
2017-11-28 10:19:41 +0100

    Updated a macro in the Libera BLM EDM GUI.

--- LiberaBLM/edm3.2/start.sh
+++ LiberaBLM/edm3.2/start.sh
@@ -11,29 +11,30 @@ IP=$2
 export EPICS_CA_ADDR_LIST=$IP
 
 export EPICS_HOST_ARCH=linux-x86
-export EPICS_TOPDIR=/home/libera/epics/base-3.14.12.2
+export EPICS_BASE=base-3.14.12.2
+export EPICS_TOPDIR=/home/libera/epics/
 
 # For large data transfers - e.g. DD data set this
 export EPICS_CA_MAX_ARRAY_BYTES=5000000
 
 # Export EPICS install bin folder to PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$EPICS_TOPDIR/lib:$EPICS_TOPDIR/lib/$EPICS_HOST_ARCH
-export PATH=$PATH:$EPICS_TOPDIR/bin/$EPICS_HOST_ARCH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$EPICS_TOPDIR/$EPICS_BASE/lib:$EPICS_TOPDIR/$EPICS_BASE/lib/$EPICS_HOST_ARCH
+export PATH=$PATH:$EPICS_TOPDIR/$EPICS_BASE/bin/$EPICS_HOST_ARCH
 
-export EDM_TOPDIR=/home/libera/GUI/LiberaBLM/edm3.2
+export EDM_TOPDIR=$(pwd)
 export EDMDATAFILES=$EDM_TOPDIR/panels
 export EDMOBJECTS=$EDM_TOPDIR/prefs
 export EDMFILES=$EDM_TOPDIR/prefs
 export EDMPVOBJECTS=$EDM_TOPDIR/prefs
-export EDMHELPFILES=$EPICS_TOPDIR/base-3.14.12.2/extensions/src/edm/helpFiles
+export EDMHELPFILES=$EPICS_TOPDIR/$EPICS_BASE/extensions/src/edm/helpFiles
 
 echo "EPICS host   : $EPICS_HOST_ARCH"
-echo "EPICS folder : $EPICS_TOPDIR"
+echo "EPICS folder : $EPICS_TOPDIR/$EPICS_BASE"
 echo "EDM panels folder : $EDMDATAFILES"
 echo "EDM prefs files  : $EDMFILES"
 
 cd $EDMDATAFILES
-edm -m "DEVICE=$DEVICE" -x main.edl
+edm -m "HOSTNAME=$DEVICE" -x main.edl
 
 exit $?
 
