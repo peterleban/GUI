c24e3309e41243f7cd7bd0a716dffb5b6bc82baf
Peter Leban <peter.leban@i-tech.si>
2018-02-14 09:14:42 +0100

    Minor updates

---
+++ LiberaHadron/caQtDM3.2/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Allow large waveform readout
+export EPICS_CA_MAX_ARRAY_BYTES=5000000
+
+# Tell EPICS clients where to look for the IOCs
+export EPICS_CA_ADDR_LIST="10.0.1.214 10.0.4.191 134.94.210.154 134.94.210.155 134.94.210.156 134.94.210.157 134.94.210.158 134.94.210.159"
+
+# Call caQtDM
+/home/libera/epics/caqtdm-3.9.4/caQtDM_Binaries/caQtDM -macro "DEVICE1=HBPM2204,DEVICE2=bplus,DEVICE3=libera03,DEVICE4=libera04,DEVICE5=libera05,DEVICE6=libera06,DEVICE7=libera07,DEVICE8=libera08,DEVICE9=libera09,DEVICE10=libera10" selection.ui
